f85013c9bc1972bfd78e1ea3e5f5068ab9c7414e
robin <zhoubiao@analysys.com.cn>
2017-03-22 17:53:49 +0800

    add qrcode

--- js/config/index.js
+++ js/config/index.js
@@ -1,1 +1,18 @@
 console.log($('body').length);
+
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+  text: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=js%E7%94%9F%E6%88%90%E4%BA%8C%E7%BB%B4%E7%A0%81&oq=qrcode.js',
+  width: 256,
+  height: 256,
+  colorDark : '#000000',
+  colorLight : '#ffffff',
+  correctLevel : QRCode.CorrectLevel.H
+});
+
+$.ajax({
+  url:'views/gard1.html',
+  type:'GET',
+  success:function (data) {
+    $('.grad-box').append(data);
+  }
+})
