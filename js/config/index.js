ce25cf7fd55249dd77203ecb0c95f7389ff01d99
robin <zhoubiao@analysys.com.cn>
2017-03-23 17:57:18 +0800

    add vue

--- js/config/index.js
+++ js/config/index.js
@@ -1,14 +1,22 @@
-console.log($('body').length);
+console.log($(document).height());
+$('.doc-height').html($(document).height());
+// var qrcode = new QRCode(document.getElementById("qrcode"), {
+//   text: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=js%E7%94%9F%E6%88%90%E4%BA%8C%E7%BB%B4%E7%A0%81&oq=qrcode.js',
+//   width: 256,
+//   height: 256,
+//   colorDark : '#000000',
+//   colorLight : '#ffffff',
+//   correctLevel : QRCode.CorrectLevel.H
+// });
 
-var qrcode = new QRCode(document.getElementById("qrcode"), {
+$("#qrcode").qrcode({
   text: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=js%E7%94%9F%E6%88%90%E4%BA%8C%E7%BB%B4%E7%A0%81&oq=qrcode.js',
   width: 256,
   height: 256,
   colorDark : '#000000',
   colorLight : '#ffffff',
   correctLevel : QRCode.CorrectLevel.H
-});
-
+})
 $.ajax({
   url:'views/gard1.html',
   type:'GET',
