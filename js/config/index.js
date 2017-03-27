b9bd0c67b80d913dc5ec24cc89d18d8ff38556e4
robin <zhoubiao@analysys.com.cn>
2017-03-27 16:17:51 +0800

    add pic

--- js/config/index.js
+++ js/config/index.js
@@ -1,5 +1,5 @@
-console.log($(document).height());
-$('.doc-height').html($(document).height());
+console.log($(document).height()+':'+window.screen.height + ':'+window.devicePixelRatio);
+$('.doc-height').html($(document).height()+':'+window.screen.height);
 // var qrcode = new QRCode(document.getElementById("qrcode"), {
 //   text: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=js%E7%94%9F%E6%88%90%E4%BA%8C%E7%BB%B4%E7%A0%81&oq=qrcode.js',
 //   width: 256,
@@ -8,6 +8,35 @@ $('.doc-height').html($(document).height());
 //   colorLight : '#ffffff',
 //   correctLevel : QRCode.CorrectLevel.H
 // });
+
+
+
+function f1(){
+    var n=999;
+  function f2(){
+   // alert(n);
+  }
+  return f2;
+}
+var result=f1();
+result(); // 999
+function test() {
+  console.log(foo);
+  console.log(bar);
+
+  var foo = 'Hello';
+  console.log(foo);
+  var bar = function () {
+    return 'world';
+  }
+
+  function foo() {
+    return 'hello';
+  }
+}
+
+test();
+
 
 $("#qrcode").qrcode({
   text: 'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&tn=baidu&wd=js%E7%94%9F%E6%88%90%E4%BA%8C%E7%BB%B4%E7%A0%81&oq=qrcode.js',
