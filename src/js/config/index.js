18de5c730e682cf644fec220a7af0cae57935051
robin <zhoubiao@analysys.com.cn>
2017-04-06 18:49:55 +0800

    add font

--- src/js/config/index.js
+++ src/js/config/index.js
@@ -1,10 +1,12 @@
 var pHome = avalon.define({
   $id:'pHome',
   buyGrade:1,
+  currentGrade:1,
   init:function () {
     var callback = function (data) {
         if(data.code === 1000){
-            
+            //pHome.currentGrade = data.data.grade;
+            addActive('.flex .item',pHome.currentGrade);
         }
     }
     GetData.getAjax('/home/wealth/member',{},callback);
@@ -14,12 +16,27 @@ var pHome = avalon.define({
   },
   choiceGrad:function (grade) {
     pHome.buyGrade = grade;
+    if(grade<pHome.currentGrade) return;
     $(this).siblings('p').removeClass('active');
-    $(this).addClass('active');
+    addActive('.flex .item',grade);
   }
 });
 pHome.init();
 
-
+function addActive(dom,grade){
+  $dom = $(dom);
+  if(grade === 1){
+    $dom.eq(0).addClass('active');
+  }else if(grade === 2){
+    $dom.eq(0).addClass('active');
+    $dom.eq(1).addClass('active');
+  }else if(grade === 3){
+    $dom.eq(0).addClass('active');
+    $dom.eq(1).addClass('active');
+    $dom.eq(2).addClass('active');
+  }else{
+    console.log('best')
+  }
+}
 
 
--- src/js/config/news.js
+++ src/js/config/news.js
@@ -1,13 +1,14 @@
 var pNews = avalon.define({
     $id:'pNews',
     currentPage:'news',
+    newsData:[],
     init:function(){
         var callback = function (data) {
             if(data.code === 1000){
-                
+                pNews.newsData = data.data.list;
             }
         }
-        GetData.getAjax('/home/wealth/personalinfo',{},callback);
+        GetData.getAjax('/home/wealth/news',{},callback);
     }
 });
 pNews.init();
